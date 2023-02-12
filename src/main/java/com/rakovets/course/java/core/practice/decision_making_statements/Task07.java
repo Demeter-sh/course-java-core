2eea87877880f774a257031e71bf3691101b0fe8
10 <reguldgreate@gmail.com>
2023-02-13 01:45:07 +0300

    feat: Dicision_making_statement Task07

--- src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task07.java
+++ src/main/java/com/rakovets/course/java/core/practice/decision_making_statements/Task07.java
@@ -23,7 +23,7 @@ class Task07 {
         //FIXME
         // Ниже приведены значения присваиваемые переменным. Их можно изменять для проверки различных вариантов входных
         // аргументов. Типы данных изменять нельзя
-        int year = 2020;
+        int year = 100;
 
         boolean isLeapYear = isLeapYear(year);
         System.out.printf("Result: %s", isLeapYear);
@@ -36,8 +36,7 @@ class Task07 {
      * @return <code>false</code>/<code>true</code>
      */
     static boolean isLeapYear(int year) {
-        //TODO
-        // Код, решающий задачу пишем ниже, при этом используя параметры метода
-        return false;
+        boolean _isLeapYear = year % 400 == 0 || year % 100 != 0 && year % 4 == 0;
+        return _isLeapYear;
     }
 }
