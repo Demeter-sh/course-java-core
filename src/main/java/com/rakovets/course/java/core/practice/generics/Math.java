a564978ae50b319a1db88932e0592e9aacdfabda
10 <reguldgreate@gmail.com>
2023-07-26 14:34:19 +0300

    feat: Generics task Math, test Math

---
+++ src/main/java/com/rakovets/course/java/core/practice/generics/Math.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.java.core.practice.generics;
+
+public class Math <T extends Number> {
+
+    //Реализуйте статический обобщенный метод, который возвращает максимум из трёх переданных параметров.
+    public static <T> T[] getThree  (T numOne, T numTwo, T numThree){
+        T[] lineNumber = new T[3];
+        return null;
+    }
+}
