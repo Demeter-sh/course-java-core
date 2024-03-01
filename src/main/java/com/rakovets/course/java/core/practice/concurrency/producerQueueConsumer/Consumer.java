0d399037971be879d86e91dcfbd0f96342050c93
10 <reguldgreate@gmail.com>
2024-03-01 20:19:20 +0300

    feat: Concurrency1.1

--- src/main/java/com/rakovets/course/java/core/practice/concurrency/producerQueueConsumer/Consumer.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/producerQueueConsumer/Consumer.java
@@ -1,8 +1,17 @@
 package com.rakovets.course.java.core.practice.concurrency.producerQueueConsumer;
 
+import com.rakovets.course.java.core.util.StandardOutputUtil;
+
 public class Consumer implements Runnable {
+    QueueContainer queueContainer;
+
+    public Consumer(QueueContainer queueContainer) {
+        this.queueContainer = queueContainer;
+    }
 
     @Override
     public void run() {
+        StandardOutputUtil.printlnWithTime("Started");
+        queueContainer.getNumbersOutOfQueue();
     }
 }
--- src/main/java/com/rakovets/course/java/core/practice/concurrency/producerQueueConsumer/QueueContainer.java
+++ src/main/java/com/rakovets/course/java/core/practice/concurrency/producerQueueConsumer/QueueContainer.java
@@ -1,16 +1,41 @@
 package com.rakovets.course.java.core.practice.concurrency.producerQueueConsumer;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Scanner;
+import java.util.logging.Logger;
 
 public class QueueContainer {
-    Queue<Integer> numbers = new PriorityQueue<>();
+    private Queue<Integer> numbers = new PriorityQueue<>();
+    public static final Logger logger = Logger.getLogger(QueueContainer.class.getName());
 
-    public Queue<Integer> getNumbers() {
-        return numbers;
+
+    public synchronized void putNumbersIntoQueue() {
+        int x = 0;
+        while (x != -1) {
+            Scanner scanner = new Scanner(System.in);
+            x = scanner.nextInt();
+            numbers.add(x);
+        }
+
     }
 
-    synchronized void addNumbersToQueue() {
-
+    public synchronized void getNumbersOutOfQueue() {
+        String numbersOutOfQueue = "";
+        try {
+            for (int i : numbers) {
+                numbersOutOfQueue = numbersOutOfQueue.concat(String.valueOf(i));
+            }
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            System.out.println(ex.getMessage());
+        }
+        try(FileOutputStream fos = new FileOutputStream("D://notes3.txt")) {
+            fos.write(numbersOutOfQueue.getBytes());
+        } catch (IOException ex){
+            logger.warning(ex.getMessage());
+        }
     }
 }
